a6959667cc493f2492d35fbd95f81d610a76bcca
EC2 Default User <ec2-user@ip-172-31-17-38.us-east-2.compute.internal>
2017-11-01 05:35:50 +0000

    first commit

---
+++ .bash_history
@@ -0,0 +1,38 @@
+sudo yum updates
+sudo yum update
+node -v
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo yum install -y nodejs
+curl -sSo- https://wiki.js.org/install.sh | bash
+node wiki start
+node wiki configure
+sudo yum install git
+sudo apt-get install git
+sudo dnf install git
+yum install git
+sudo yum install git
+node wiki configure
+node wiki start
+npm install
+node -v
+node server
+sudo node server
+ls
+node server
+node wiki
+node start
+node wiki start
+node wiki -V
+node wiki --version
+node wiki restart
+ls
+node restart
+node wiki restart
+sudo node wiki restart
+node wiki restart
+node wiki start
+node wiki stop
+sudo node wiki start
---
+++ server/models/user.js
@@ -0,0 +1,109 @@
+'use strict'
+
+/* global db, lang */
+
+const Mongoose = require('mongoose')
+const Promise = require('bluebird')
+const bcrypt = require('bcryptjs-then')
+const _ = require('lodash')
+
+/**
+ * Users schema
+ *
+ * @type       {<Mongoose.Schema>}
+ */
+var userSchema = Mongoose.Schema({
+
+  email: {
+    type: String,
+    required: true,
+    index: true
+  },
+
+  provider: {
+    type: String,
+    required: true
+  },
+
+  providerId: {
+    type: String
+  },
+
+  password: {
+    type: String
+  },
+
+  name: {
+    type: String
+  },
+
+  rights: [{
+    role: String,
+    path: String,
+    exact: Boolean,
+    deny: Boolean
+  }]
+
+}, { timestamps: {} })
+
+userSchema.statics.processProfile = (profile) => {
+  let primaryEmail = ''
+  if (_.isArray(profile.emails)) {
+    let e = _.find(profile.emails, ['primary', true])
+    primaryEmail = (e) ? e.value : _.first(profile.emails).value
+  } else if (_.isString(profile.email) && profile.email.length > 5) {
+    primaryEmail = profile.email
+  } else if (_.isString(profile.mail) && profile.mail.length > 5) {
+    primaryEmail = profile.mail
+  } else if (profile.user && profile.user.email && profile.user.email.length > 5) {
+    primaryEmail = profile.user.email
+  } else {
+    return Promise.reject(new Error(lang.t('auth:errors.invaliduseremail')))
+  }
+
+  profile.provider = _.lowerCase(profile.provider)
+  primaryEmail = _.toLower(primaryEmail)
+
+  return db.User.findOneAndUpdate({
+    email: primaryEmail,
+    provider: profile.provider
+  }, {
+    email: primaryEmail,
+    provider: profile.provider,
+    providerId: profile.id,
+    name: profile.displayName || profile.cn || _.split(primaryEmail, '@')[0]
+  }, {
+    new: true
+  }).then((user) => {
+    // Handle unregistered accounts
+    if (!user && profile.provider !== 'local' && (appconfig.auth.defaultReadAccess || profile.provider === 'ldap' || profile.provider === 'azure')) {
+      let nUsr = {
+        email: primaryEmail,
+        provider: profile.provider,
+        providerId: profile.id,
+        password: '',
+        name: profile.displayName || profile.name || profile.cn,
+        rights: [{
+          role: 'read',
+          path: '/',
+          exact: false,
+          deny: false
+        }]
+      }
+      return db.User.create(nUsr)
+    }
+    return user || Promise.reject(new Error(lang.t('auth:errors:notyetauthorized')))
+  })
+}
+
+userSchema.statics.hashPassword = (rawPwd) => {
+  return bcrypt.hash(rawPwd)
+}
+
+userSchema.methods.validatePassword = function (rawPwd) {
+  return bcrypt.compare(rawPwd, this.password).then((isValid) => {
+    return (isValid) ? true : Promise.reject(new Error(lang.t('auth:errors:invalidlogin')))
+  })
+}
+
+module.exports = Mongoose.model('User', userSchema)
